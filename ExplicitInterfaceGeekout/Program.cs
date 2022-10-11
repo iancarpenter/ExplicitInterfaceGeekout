bcc25645d2514bbf1f9dc3d67787301ba738a102
iancarpenter <ianhcarpenter@gmail.com>
2022-10-11 19:34:10 +0100

    Add project files.

---
+++ ExplicitInterfaceGeekout/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ExplicitInterfaceGeekout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ExplodingBalloon explodingBalloon = new ExplodingBalloon();
+
+            IBomb bomb = explodingBalloon;
+            bomb.BlowUp();
+
+            IBalloon balloon = explodingBalloon;
+            balloon.BlowUp();
+
+        }
+    }
+
+
+    public class ExplodingBalloon : IBomb, IBalloon
+    {
+        void IBomb.BlowUp() { Console.WriteLine("Kaboom!"); }
+        void IBalloon.BlowUp() { Console.WriteLine("Whoosh"); }
+
+    }
+
+    public interface IBomb { void BlowUp(); }
+    public interface IBalloon { void BlowUp(); }
+}
